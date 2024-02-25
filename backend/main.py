3ac8abfb46f96aad3d8cecb66ba259187a08593c
Chris Spann <cspanndev@gmail.com>
2024-02-25 11:09:47 -0600

    pin python3.11

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
 
-    logger.info("Starting uvicorn in reload mode")
+    logger.info("Starting uvicorn in reload mode.")
     uvicorn.run(
         "main:app",
         host="0.0.0.0",

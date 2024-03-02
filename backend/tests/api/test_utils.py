71a7fa845162f1f1d8a1d3f693d781510a8c57a6
Chris Spann <cspanndev@gmail.com>
2024-03-02 17:22:43 -0600

    scope to module

--- backend/tests/api/test_utils.py
+++ backend/tests/api/test_utils.py
@@ -4,7 +4,7 @@ from httpx import AsyncClient
 from app.core.config import settings
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="module")
 async def test_hello_world(client: AsyncClient) -> None:
     resp = await client.get(f"{settings.API_PATH}/hello-world")
     data = resp.json()
--- backend/tests/models/test_user.py
+++ backend/tests/models/test_user.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="module")
 async def test_user_model(db: AsyncSession):
     user = User(id=uuid4(), email="test@example.com", hashed_password="1234")
     db.add(user)

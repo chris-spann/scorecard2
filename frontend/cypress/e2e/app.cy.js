51b59b43c434607564ec50ee2eb6c502d2035b51
Chris Spann <cspanndev@gmail.com>
2024-03-15 14:25:10 -0500

    try again

--- frontend/cypress/e2e/app.cy.js
+++ frontend/cypress/e2e/app.cy.js
@@ -28,7 +28,8 @@ describe("Test register, login and item", () => {
   it("Logs in", () => {
     // if Welcome to admin already displayed anywhere, then no need to login
     cy.get("body").then(($body) => {
-      if ($body.text().includes("Welcome to admin")) {
+      // if ($body.innertext().includes("Welcome to admin")) {
+      if (cy.contains($body, "Welcome to admin").length > 0) {
         return;
       }
     });

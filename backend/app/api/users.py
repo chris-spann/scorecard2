3b1ba5ae69d877ea90921dd05c7a059e7c2ef748
Chris Spann <cspanndev@gmail.com>
2024-03-17 10:41:20 -0500

    more cleanup

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,13 +1,13 @@
 from typing import Any
 
-from fastapi import Depends
+from fastapi import Depends, params
 from fastapi.routing import APIRouter
 from fastapi_pagination import Page
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from app.deps.db import get_async_session
+from app.deps.db import CurrentAsyncSession, get_async_session
 from app.deps.users import CurrentSuperuser
 from app.models.user import User
 from app.schemas.user import UserRead
@@ -16,9 +16,6 @@ router = APIRouter(prefix="/users")
 
 
 @router.get("", response_model=Page[UserRead])
-async def get_paginated_users(
-    user: CurrentSuperuser,
-    session: AsyncSession = Depends(get_async_session),
-) -> Any:
+async def get_paginated_users(user: CurrentSuperuser, session: CurrentAsyncSession) -> Any:
     query = select(User).order_by(User.created)
     return await paginate(session, query)
